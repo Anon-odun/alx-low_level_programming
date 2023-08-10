836468b2ddf9a74161cff8f187218b95293fb9fb
Tosin <ebitighaoluwatosin@gmail.com>
2023-08-10 21:29:24 +0000

    Function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *puts - writes the string s and a trailing newline to stdout
+ *@str - string to be written
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
