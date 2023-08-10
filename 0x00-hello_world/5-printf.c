e7fca124e0c37551b6bee356ae19555f806b7989
Tosin <ebitighaoluwatosin@gmail.com>
2023-08-10 21:50:40 +0000

    Function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *printf - sends formatted output to stdout
+ *@format - string that contains the text to be written to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
