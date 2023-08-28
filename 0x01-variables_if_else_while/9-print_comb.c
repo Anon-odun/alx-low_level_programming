1d9f7b092d63052b5cd6bed5baddb4745e11d3fb
Tosin <ebitighaoluwatosin@gmail.com>
2023-08-28 21:49:13 +0000

    Print comb of single numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of single-digit numbers
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+{
+	putchar('0' + i);
+
+{
+	if (i <= 8)
+{	putchar(',');
+	putchar(' ');
+}
+}
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
